d58515ba027d92b7c50ec3b2b2e9d1d985f841c3
Darian Weingaertner <darian.weingaertner@gmail.com>
2024-05-11 22:57:34 +0200

    Unterkomponente TaskList.vue erstellt, die eine Liste von Aufgaben mit v-for rendert.

---
+++ src/components/TaskList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">{{ task.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const tasks = ref([
+  { id: 1, name: 'Staubsaugen' },
+  { id: 2, name: 'Fenster putzen' },
+  { id: 3, name: 'Bad reinigen' }
+]);
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Willkommen zum PutzPlaner!</h1>
+    <task-list></task-list>
+  </div>
 </template>
+
+<script setup>
+import TaskList from '../components/TaskList.vue';
+</script>
