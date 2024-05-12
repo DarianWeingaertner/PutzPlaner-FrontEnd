4f913c7fa8a2e77eb92b4d45f1f66e6dece71587
Darian Weingaertner <darian.weingaertner@gmail.com>
2024-05-12 11:05:44 +0200

    TaskList kann jetzt abgehakt werden

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <ul>
-      <li v-for="task in tasks" :key="task.id">{{ task.name }}</li>
+      <li v-for="task in tasks" :key="task.id">
+        <input type="checkbox" @change="toggleTask(task)" :checked="task.completed">
+        <span v-if="!task.completed">{{ task.name }}</span>
+      </li>
     </ul>
   </div>
 </template>
@@ -10,8 +13,11 @@
 import { ref } from 'vue';
 
 const tasks = ref([
-  { id: 1, name: 'Staubsaugen' },
-  { id: 2, name: 'Fenster putzen' },
-  { id: 3, name: 'Bad reinigen' }
+  { id: 1, name: 'Staubsaugen', completed: false },
+  { id: 2, name: 'Fenster putzen', completed: false },
+  { id: 3, name: 'Bad reinigen', completed: false }
 ]);
+function toggleTask(task) {
+  task.completed = !task.completed;
+}
 </script>
