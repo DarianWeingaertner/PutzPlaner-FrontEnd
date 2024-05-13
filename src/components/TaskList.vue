ae4aa35e528c69bd20bfe974541329d80a9be9c8
Darian Weingaertner <darian.weingaertner@gmail.com>
2024-05-13 22:51:43 +0200

    TaskList kann jetzt neue Aufgaben aufnehmen in dem man im Texteingabefeld eine Aufgabe anlegt

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,23 +1,103 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+type Task = { id: number; name: string }
+type TaskListOaData = {
+  tasks: Task[]
+  nameField: string
+  currentId: number
+}
+
+export default defineComponent({
+  name: 'TaskOptionsApiList',
+  props: ['title'],
+  data(): TaskListOaData {
+    return {
+      nameField: '',
+      currentId: 1,
+      tasks: []
+    }
+  },
+  methods: {
+    initTask(): void {
+      this.addTask('Fenster säubern')
+      this.addTask('Müll rausbringen')
+    },
+    addTask(name: string): void {
+      this.tasks.push({name, id: this.currentId++})
+    },
+    onFormSubmitted(): void {
+      this.addTask(this.nameField.valueOf())
+    },
+    removeTask(id: number): void {
+      this.tasks = this.tasks.filter((h) => h.id !== id)
+    }
+  },
+  mounted() {
+    this.initTask()
+  }
+})
+</script>
+
 <template>
-  <div>
-    <ul>
-      <li v-for="task in tasks" :key="task.id">
-        <input type="checkbox" @change="toggleTask(task)" :checked="task.completed">
-        <span v-if="!task.completed">{{ task.name }}</span>
-      </li>
-    </ul>
+  <div class="container">
+    <h3>Deine heutigen Aufgaben sind:</h3>
+    <table>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>ID</th>
+      </tr>
+      <tr v-if="!tasks.length">
+        <td colspan="3">Keine aktuellen Aufgaben!</td>
+      </tr>
+      <tr v-for="task in tasks" :key="task.id">
+        <td>
+          <button @click="removeTask(task.id)" class="delete">erledigt</button>
+        </td>
+        <td>{{ task.name }}</td>
+        <td>({{ task.id }})</td>
+      </tr>
+    </table>
+    <h2>{{ title }}</h2>
+    <form @submit="onFormSubmitted()" @submit.prevent>
+      <!-- "@submit.prevent" prevents a page refresh after submitting form -->
+      <input type="text" placeholder="Aufgabe eingeben" v-model="nameField"/>
+      <button>speichern</button>
+    </form>
+    <hr/>
   </div>
 </template>
 
-<script setup>
-import { ref } from 'vue';
 
-const tasks = ref([
-  { id: 1, name: 'Staubsaugen', completed: false },
-  { id: 2, name: 'Fenster putzen', completed: false },
-  { id: 3, name: 'Bad reinigen', completed: false }
-]);
-function toggleTask(task) {
-  task.completed = !task.completed;
+<style scoped>
+form {
+  display: flex;
+  margin: 0 -16px;
+
+  * {
+    margin: 16px;
+  }
+
+  input {
+    flex-grow: 1;
+  }
 }
-</script>
+
+table {
+  margin: 8px -8px 0;
+
+  th,
+  td {
+    padding: 8px;
+  }
+}
+
+button {
+  border-radius: 48px;
+  padding: 6px;
+  border: none;
+  cursor: pointer;
+
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
     <h1>Willkommen zum PutzPlaner!</h1>
-    <h2>Deine heutigen Aufgaben sind:</h2>
-    <task-list></task-list>
+    <task-options-api-list></task-options-api-list>
   </div>
 </template>
 
 <script setup>
-import TaskList from '../components/TaskList.vue';
+import TaskOptionsApiList from '../components/TaskList.vue';
 </script>
