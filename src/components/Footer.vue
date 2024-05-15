8627c73254f94c965e922aa24e8e90ae7a8ade4e
Darian Weingaertner <darian.weingaertner@gmail.com>
2024-05-15 12:56:49 +0200

    Footer angepasst

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<footer class="bg-body-tertiary">
+<p>PutzPlaner</p>
+</footer>
+
+<style scoped>
+footer.bg-body-tertiary {
+  min-height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 20px 0;
+}
+</style>
+<script setup lang="ts">
+</script>
