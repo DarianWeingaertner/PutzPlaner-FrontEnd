d399e1fa4077d5f2e62c3acfaf46b552a5bf9f39
Philipp Sta <stami1231@web.de>
2024-06-30 22:52:26 +0200

    neue Info

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -3,32 +3,30 @@
 </script>
 
 <template>
-  <template>
-    <h1>Nutzung der PutzPlaner-App</h1>
+    <h2>Nutzung der PutzPlaner-App</h2>
     <p>PutzPlaner ist eine Aufgabenmanagement-App zur Verwaltung von Reinigungstätigkeiten. Nutzer können Aufgaben hinzufügen, löschen und als erledigt markieren. Die App hat eine intuitive Benutzeroberfläche und basiert auf Vue.js für das Frontend und Spring Boot für das Backend.</p>
 
-    <h2>Startseite</h2>
+    <h3>Startseite</h3>
     <p>Öffne die App im Browser unter <a href="https://putzplaner-frontend.onrender.com">https://putzplaner-frontend.onrender.com</a>.</p>
     <p>Du siehst eine Übersicht aller aktuellen Aufgaben.</p>
 
-    <h2>Aufgaben hinzufügen</h2>
+    <h3>Aufgaben hinzufügen</h3>
     <p>Fülle die Felder für "Bezeichnung", "Person" und "Tage" aus.</p>
     <p>Klicke auf "Speichern", um die Aufgabe hinzuzufügen.</p>
 
-    <h2>Aufgaben verwalten</h2>
+    <h3>Aufgaben verwalten</h3>
     <p><strong>Erledigen</strong>: Klicke auf "Erledigt" neben einer Aufgabe, um sie als abgeschlossen zu markieren.</p>
     <p><strong>Löschen</strong>: Klicke auf "Löschen", um eine Aufgabe zu entfernen.</p>
     <p><strong>Anpassen</strong>: Klicke auf "Anpassen", um sie nachträglich zu ändern.</p>
 
-    <h2>Navigation</h2>
+    <h3>Navigation</h3>
     <p>Über die Navigationsleiste kannst du zu den Seiten "Startseite", "Historie" und "Info" wechseln.</p>
 
-    <h2>Historie</h2>
+    <h3>Historie</h3>
     <p>In der Historie werden abgeschlossene Aufgaben angezeigt.</p>
 
     <h3>Darkmode</h3>
     <p>Klicke auf den "Dark Mode Button", um zwischen dem Hell- und Dunkelmodus zu wechseln.</p>
-  </template>
 
 </template>
 
